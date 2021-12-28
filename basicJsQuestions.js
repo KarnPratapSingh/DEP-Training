d70334db665350ff7f8db3f5046529a4cf765eaf
KarnPratapSingh <kpsingh8960@gmail.com>
2021-12-28 17:33:31 +0530

    changes after first review

--- basicJsQuestions.js
+++ basicJsQuestions.js
@@ -1,70 +1,51 @@
 //QUESTION 1:
 // input: ["apple","banana","orange"]
 //   output:"apple,banana,orange"
-  
 
-
-function printArrayInStringFormat(array){
-    //show me the code!
-    var stringFromArray=array.toString();
-    return stringFromArray;
-  }
-
-
-
-
-
-
+function printArrayInStringFormat(array) {
+  //show me the code!
+  var stringFromArray = array.toString();
+  return stringFromArray;
+}
 
 //QUESTION 2:
 // input : -2,3,-23
 // output : 2,-3,23
 
 function opposite(number) {
-    //your code here
-    var newNumber;
-    if(number>0){
-      newNumber=number-(2*number);
-      return newNumber;
-    }
-    else if(number<0){
-      newNumber=number+(2*Math.abs(number));
-      return newNumber
-    }
-    else{
-      return number;
-    }
+  //your code here
+  var newNumber;
+  if (number > 0) {
+    newNumber = number - 2 * number;
+    return newNumber;
+  } else if (number < 0) {
+    newNumber = Math.abs(number);
+    return newNumber;
+  } else {
+    return number;
   }
-  
-  
+}
 
 // QUESTION 3:
 // input: basicOp('+',1,2);
 // output: 3
 
-function basicOp(operation, value1, value2)
-  {
-    // Code
-    if(operation=='+'){
-      return value1+value2;
-    }
-    else if(operation=='-'){
-      return value1-value2;
-    }
-    else if(operation=='*'){
-      return value1*value2;
-    }
-    else if(operation=='/'){
-      return value1/value2;
-    }
-    else{
-      return 'Invalid operation';
-    }
+function basicOp(operation, value1, value2) {
+  // Code
+  if (operation == "+") {
+    return value1 + value2;
+  } else if (operation == "-") {
+    return value1 - value2;
+  } else if (operation == "*") {
+    return value1 * value2;
+  } else if (operation == "/") {
+    return value1 / value2;
+  } else {
+    return "Invalid operation";
   }
-  
-basicOp('+',1,2);
+}
 
-
+basicOp("+", 1, 2);
 
 //QUESTION 4:
 /*  After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
@@ -75,18 +56,13 @@ Every day you rent the car costs $40. If you rent the car for 7 or more days, yo
 
 Write a code that gives out the total amount for different days(d).*/
 
-
-
 function rentalCarCost(days) {
-    // Your solution here
-    if(days>=7){
-      return ((d*40)-50);
-    }
-    else if(days>=3){
-      return ((d*40)-20);
-    }
-    else{
-      return (days*40);
-    }
+  // Your solution here
+  if (days >= 7) {
+    return days * 40 - 50;
+  } else if (days >= 3) {
+    return days * 40 - 20;
+  } else {
+    return days * 40;
   }
- 
+}
